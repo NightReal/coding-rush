8ef869116d128d94760fe94434edc65edeb85c94
crazyilian <ilian.and.04@gmail.com>
2020-09-11 00:37:04 +0300

    Added stop button. Refactoring.

--- main.js
+++ main.js
@@ -1,10 +1,16 @@
 let editor = document.getElementById("editor");
 let target = document.getElementById("target");
-let button = document.getElementById("startButton");
+let startButton = document.getElementById("startButton");
+let stopButton = document.getElementById("stopButton");
 let targetText = "Hello, world!";
 let status = document.getElementById("status");
 let startTime = undefined;
 let typingTimer;
+
+
+window.onload = function() {
+    target.value = targetText;
+};
 
 
 function time2secs(tm) {
@@ -21,27 +27,36 @@ function showTypingTimer() {
 } 
 
 
-window.onload = function() {
-    target.value = targetText;
-};
-
-
-button.onclick = function() {
+startButton.onclick = function() {
     editor.value = "";
     startTime = new Date();
     showTypingTimer();
     typingTimer = setInterval(showTypingTimer, 100);
     editor.readOnly = false;
     editor.focus();
+    stopButton.disabled = false;
+}
+
+
+function endTyping() {
+    clearInterval(typingTimer);
+    startTime = undefined;
+    editor.readOnly = true;
+    stopButton.disabled = true;
+}
+
+
+stopButton.onclick = function() {
+    status.innerHTML = "You stopped typing.";
+    endTyping();
 }
 
 
 editor.oninput = function() {
     if (editor.value == targetText) {
-        clearInterval(typingTimer);
-        status.innerHTML = "Done! Your time: " + time2secs(new Date() - startTime) + "s";
-        startTime = undefined;
-        editor.readOnly = true;
+        var tm = time2secs(new Date() - startTime);
+        endTyping();
+        status.innerHTML = "Done! Your time: " + tm + "s";
     }
 };
 
