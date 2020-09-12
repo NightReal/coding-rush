b96e8a52181cbe7d3bed072aa4387ea23e6befab
crazyilian <ilian.and.04@gmail.com>
2020-09-12 11:00:23 +0300

    One button for start and stop instead of two.

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
 let editor = document.getElementById("editor");
 let target = document.getElementById("target");
 let startButton = document.getElementById("startButton");
-let stopButton = document.getElementById("stopButton");
 let targetText = "Hello, world!";
 let status = document.getElementById("status");
 let startTime = undefined;
 let typingTimer;
+let typing = false;
 
 
 window.onload = function() {
@@ -19,44 +19,42 @@ function time2secs(tm) {
 
 
 function showTypingTimer() {
-    if (typeof startTime == "undefined") {
+    if (typeof startTime == "undefined")
         return;
-    }
     var tm = time2secs(new Date() - startTime);
     status.innerHTML = "You type: " + tm + "s";
 } 
 
-
-startButton.onclick = function() {
-    editor.value = "";
-    startTime = new Date();
-    showTypingTimer();
-    typingTimer = setInterval(showTypingTimer, 100);
-    editor.readOnly = false;
-    editor.focus();
-    stopButton.disabled = false;
+startButton.onclick = function(e, rightText=false) {
+    startButton.classList.toggle('darkred-btn');
+    startButton.classList.toggle('green-btn');
+    editor.readOnly ^= true;
+    if (typing) {
+        startButton.innderHTML = "Start";
+        clearInterval(typingTimer);
+        if (!rightText)
+            status.innerHTML = "You stopped typing.";
+        else {
+            var tm = time2secs(new Date() - startTime);
+            status.innerHTML = "Done! Your time: " + tm + "s";
+        }
+        startTime = undefined;
+    } else {
+        startButton.innderHTML = "Stop";
+        editor.value = "";
+        startTime = new Date();
+        showTypingTimer();
+        typingTimer = setInterval(showTypingTimer, 100);
+        editor.focus();
+    }
+    typing ^= true;
 }
 
 
-function endTyping() {
-    clearInterval(typingTimer);
-    startTime = undefined;
-    editor.readOnly = true;
-    stopButton.disabled = true;
-}
 
-
-stopButton.onclick = function() {
-    status.innerHTML = "You stopped typing.";
-    endTyping();
-}
-
-
-editor.oninput = function() {
+editor.oninput = function(e) {
     if (editor.value == targetText) {
-        var tm = time2secs(new Date() - startTime);
-        endTyping();
-        status.innerHTML = "Done! Your time: " + tm + "s";
+        startButton.onclick(e, rightText=true);
     }
 };
 
