afebe716b6d08d1a0b34d074d00d06984f04a1bc
crazyilian <ilian.and.04@gmail.com>
2020-09-13 14:14:02 +0300

    Added dynamic text name: if it is too long, it will move.

--- main.js
+++ main.js
@@ -6,11 +6,34 @@ let status = document.getElementById("status");
 let startTime = undefined;
 let typingTimer;
 let typing = false;
+let stylesheet = document.documentElement.appendChild(document.createElement("style")).sheet;
+let textName = document.getElementById("textName");
+let textNameText = textName.innerHTML;
+let textNameWidth = textName.clientWidth;
 
 
-window.onload = function() {
-    target.value = targetText;
-};
+function updateTextNameAnimation() {
+    let textNameFieldWidth = document.getElementById("textNameField").width;
+    if (textNameWidth <= textNameFieldWidth) {
+        textName.style.animationName = "";
+        textName.innerHTML = textNameText;
+        return;
+    }
+    textName.innerHTML = (textNameText + "&nbsp;".repeat(8)).repeat(2);
+    widthText = textName.clientWidth / 2;
+    var rule = ` example {
+        0% { left: 0px; }
+        15% { left: 0px; }
+        100% { left: ${-widthText}px; }
+    }`;
+    if (CSSRule.KEYFRAMES_RULE)
+        rule = "@keyframes" + rule;
+    else 
+        rule = "@-webkit-keyframes" + rule;
+    stylesheet.insertRule(rule, stylesheet.cssRules.length);
+    textName.style.animationName = "example";
+    textName.style.animationDuration = (widthText / 60) + 's';
+}
 
 
 function time2secs(tm) {
@@ -24,6 +47,18 @@ function showTypingTimer() {
     var tm = time2secs(new Date() - startTime);
     status.innerHTML = "You type: " + tm + "s";
 } 
+
+
+window.onload = function() {
+    target.value = targetText;
+    updateTextNameAnimation();
+}
+
+
+window.onresize = function() {
+    updateTextNameAnimation();
+}
+
 
 startButton.onclick = function(e, rightText=false) {
     startButton.classList.toggle('darkred-btn');
@@ -51,7 +86,6 @@ startButton.onclick = function(e, rightText=false) {
 }
 
 
-
 editor.oninput = function(e) {
     if (editor.value == targetText) {
         startButton.onclick(e, rightText=true);
@@ -67,4 +101,5 @@ target.oncopy = function(event) {
 target.onselect = function() { // deselect
   target.selectionStart = target.selectionEnd;
 };
+
 
