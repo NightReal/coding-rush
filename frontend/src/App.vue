696906a1b32cc77516eb90eb9b0cb7140e380b37
eyakm1 <abw00001@ya.ru>
2020-10-25 00:37:54 +0300

    Updated structure

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,8 +1,28 @@
 <template>
   <div id="app">
+    <div id="header">
+      <Header/>
+    </div>
     <router-view/>
+    <div id="footer">
+      <Footer/>
+    </div>
   </div>
 </template>
+
+<script>
+import Header from '@/components/Header.vue';
+import Footer from '@/components/Footer.vue';
+
+export default {
+  components: {
+    Header,
+    Footer,
+  },
+  name: 'coding-rush',
+};
+
+</script>
 
 <style lang="scss">
 #app {
