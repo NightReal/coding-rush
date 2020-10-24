be5e5eb0ec6f7d08bdb68fa157ad79d90f287335
Alexander Nekrasov <alexspeedcuber@gmail.com>
2020-10-24 12:11:15 +0300

    Partially rewrited frontend in Vue.js

---
+++ frontend/coding-rush/src/components/Editor.vue
@@ -0,0 +1,27 @@
+<template>
+    <p>
+        ajsdlkfjsadklf
+    </p>
+</template>
+
+<script>
+export default {
+  name: 'Footer',
+  props: ['text']
+}
+</script>
+
+<style scoped>
+.headfoot {
+    background-color: #300058;
+    color: white;
+    clear: both;
+}
+
+#copyright {
+    position: relative;
+    margin-left: 12px;
+    top: 2px;
+    font: inherit;
+}
+</style>
