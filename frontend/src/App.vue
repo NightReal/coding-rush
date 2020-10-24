713f004cc65fe9b24928309dea4fe53aa9094e56
eyakm1 <abw00001@ya.ru>
2020-10-25 00:21:15 +0300

    Fixed Vue structure

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,41 +1,15 @@
 <template>
-  <div id="app"
-       style="display: flex; flex-direction: column; height: 100%; justify-content: space-between;">
-    <!-- <img src="./assets/logo.png"> -->
-    <Header/>
-    <Editor :text="text"></Editor>
-    <Footer style="flex-shrink: 0;"></Footer>
+  <div id="app">
+    <router-view/>
   </div>
 </template>
 
-<script>
-import Header from './components/Header.vue';
-import Footer from './components/Footer.vue';
-import Editor from './components/Editor.vue';
-
-export default {
-  name: 'App',
-  components: {
-    Header,
-    Footer,
-    Editor,
-  },
-  data() {
-    return {
-      text: 'Hello, world!',
-    };
-  },
-};
-</script>
-
-<style>
+<style lang="scss">
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-}
-
-body {
-  margin: 0;
+  text-align: center;
+  color: #2c3e50;
 }
 </style>
--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,18 +1,41 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app"
+       style="display: flex; flex-direction: column; height: 100%; justify-content: space-between;">
+    <!-- <img src="./assets/logo.png"> -->
+    <Header/>
+    <Editor :text="text"></Editor>
+    <Footer style="flex-shrink: 0;"></Footer>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue';
+import Header from '@/components/Header.vue';
+import Footer from '@/components/Footer.vue';
+import Editor from '@/components/Editor.vue';
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld,
+    Header,
+    Footer,
+    Editor,
+  },
+  data() {
+    return {
+      text: 'Hello, world!',
+    };
   },
 };
 </script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
+
+body {
+  margin: 0;
+}
+</style>
