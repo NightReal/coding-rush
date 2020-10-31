21748f0c4e43a87ed36bc9ec99bbd5ea0084dba7
eyakm1 <abw00001@ya.ru>
2020-10-31 19:40:58 +0300

    Fix icons

---
+++ frontend/vue.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  pwa: {
+    themeColor: '#ffffff',
+    msTileColor: '#da532c',
+    appleMobileWebAppCapable: 'yes',
+    appleMobileWebAppStatusBarStyle: '#ea8c00',
+    manifestPath: 'site.webmanifest',
+    iconPath: {
+      favicon32: 'img/icons/favicon-16x16.png',
+      favicon16: 'img/icons/favicon-32x32.png',
+      appleTouchIcon: 'img/icons/apple-touch-icon.png',
+      msTileImage: 'img/icons/mstile-144x144.png',
+      maskIcon: 'img/icons/safari-pinned-tab.svg',
+    },
+  },
+};
