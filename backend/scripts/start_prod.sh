7c14ec46adec8c0df1d0357de16dc94cfc3af87a
eyakm1 <abw00001@ya.ru>
2020-11-09 00:07:10 +0300

    Prob final fix

--- backend/scripts/start_prod.sh
+++ backend/scripts/start_prod.sh
@@ -4,4 +4,4 @@ cd backend
 python3 manage.py collectstatic --no-input
 python3 manage.py makemigrations
 python3 manage.py migrate --no-input
-gunicorn backend.wsgi -b 0.0.0.0:8000 --log-level=debug --worker-class eventlet
+gunicorn backend.wsgi -b 0.0.0.0:8000
