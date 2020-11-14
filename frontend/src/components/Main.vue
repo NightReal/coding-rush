d208adba10e6fcd90048bb21c11da0cbff8dd27c
Alexander Nekrasov <alexspeedcuber@gmail.com>
2020-11-14 12:41:17 +0300

    Added navbar and simple home page

---
+++ frontend/src/components/Main.vue
@@ -0,0 +1,14 @@
+<template>
+  <v-container>
+    just some text
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: 'Main',
+};
+</script>
+
+<style scoped>
+</style>
--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,23 +1,17 @@
 <template>
   <!-- <img src="./assets/logo.png"> -->
   <v-container>
-    <Editor :textProp="text" :textNameProp="textName"></Editor>
+    <Main/>
   </v-container>
 </template>
 
 <script>
-import Editor from '@/components/Editor.vue';
+import Main from '@/components/Main.vue';
 
 export default {
   name: 'Home',
   components: {
-    Editor,
-  },
-  data() {
-    return {
-      text: '#include <bits/stdc++.h>\nusing namespace std;\n\n\nint main() {\n    ios_base::sync_with_stdio(0);\n    cin.tie(0);\n    cout.tie(0);\n    int n;\n    cin >> n;\n    return 0;\n}\n',
-      textName: 'First step - Hello, world!',
-    };
+    Main,
   },
 };
 </script>
