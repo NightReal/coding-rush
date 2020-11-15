c783ce2edc5efd55635bbd8d9152bc81927bba1c
eyakm1 <abw00001@ya.ru>
2020-11-15 14:24:04 +0300

    Snippets models and API setup

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -18,5 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('api/account/', include('accounts.urls')),
+    path('api/snippets/', include('snippets.urls')),
     path('admin/', admin.site.urls),
 ]
