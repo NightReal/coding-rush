a29b62b77ce4060123ed678cfe901f09408a84e5
eyakm1 <abw00001@ya.ru>
2020-11-19 22:44:39 +0300

    Login and register setup

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,7 +1,10 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
+import store from '../store/index';
 import Home from '../views/Home.vue';
 import EditorView from '../views/EditorView.vue';
+import Login from '../views/Login.vue';
+import Register from '../views/Register.vue';
 
 Vue.use(VueRouter);
 
@@ -10,11 +13,24 @@ const routes = [
     path: '/',
     name: 'Home',
     component: Home,
+    meta: {
+      requiresAuth: true,
+    },
   },
   {
     path: '/editor',
     name: 'Editor',
     component: EditorView,
+  },
+  {
+    path: '/login',
+    name: 'Login',
+    component: Login,
+  },
+  {
+    path: '/register',
+    name: 'Register',
+    component: Register,
   },
 ];
 
@@ -22,6 +38,18 @@ const router = new VueRouter({
   mode: 'history',
   base: process.env.BASE_URL,
   routes,
+});
+
+router.beforeEach((to, from, next) => {
+  if (to.matched.some((record) => record.meta.requiresAuth)) {
+    if (store.getter.isLoggedIn) {
+      next();
+      return;
+    }
+    next('/login');
+  } else {
+    next();
+  }
 });
 
 export default router;
---
+++ frontend/src/views/Login.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <form class="login" @submit.prevent="login">
+      <h1>Sign in</h1>
+      <label>Email</label>
+      <input required v-model="email" type="email" placeholder="E-mail"/>
+      <label>Password</label>
+      <input required v-model="password" type="password" placeholder="Password"/>
+      <hr/>
+      <button type="submit">Login</button>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Login.vue',
+  data() {
+    return {
+      email: '',
+      password: '',
+    };
+  },
+  methods: {
+    login() {
+      const { email } = this;
+      const { password } = this;
+      this.$store.dispatch('login', { email, password })
+        .then(() => this.$router.push('/'))
+        .catch((err) => console.log(err));
+    },
+  },
+};
+</script>
+
+<style scoped>
+
+</style>
