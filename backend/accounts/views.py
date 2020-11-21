fc21cc1375b5ccaf4938a54759b9f655d0f4c9bf
eyakm1 <abw00001@ya.ru>
2020-11-21 03:22:39 +0300

    Profile model, token refresh, api helper, vuex store fixes and improvements

--- backend/accounts/views.py
+++ backend/accounts/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from django.contrib.auth.models import User
+from .serializers import UserSerializer
+from .permissions import UserAccountViewPermission
+from rest_framework import views, response
+
 
 # Create your views here.
+
+class AccountView(views.APIView):
+    queryset = User.objects.all()
+    permission_classes = (UserAccountViewPermission,)
+
+    def get(self, request, format=None):
+        user = request.user
+        serializer = UserSerializer(user, many=False)
+        return response.Response(serializer.data)
