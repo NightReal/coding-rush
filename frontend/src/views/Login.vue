73dad9d15f327a2d8cc15e7f8d07e8b30358ba9f
CrazyIlian <ilian.and.04@gmail.com>
2020-11-28 20:01:21 +0300

    Sign-in form connected to query.

--- frontend/src/views/Login.vue
+++ frontend/src/views/Login.vue
@@ -1,15 +1,6 @@
 <template>
   <div>
     <LoginForm/>
-    <form class="login" @submit.prevent="login">
-      <h1>Sign in</h1>
-      <label>Username</label>
-      <input required v-model="username" type="text" placeholder="Username"/>
-      <label>Password</label>
-      <input required v-model="password" type="password" placeholder="Password"/>
-      <hr/>
-      <button type="submit">Login</button>
-    </form>
   </div>
 </template>
 
@@ -19,22 +10,6 @@ import LoginForm from '@/components/LoginForm/LoginForm.vue';
 export default {
   name: 'Login.vue',
   components: { LoginForm },
-  data() {
-    return {
-      password: '',
-      username: '',
-    };
-  },
-  methods: {
-    login() {
-      const { username } = this;
-      const { password } = this;
-      this.$store.dispatch('login', { username, password })
-        .then(() => this.$router.push('/'))
-        // eslint-disable-next-line no-console
-        .catch((err) => console.log(err));
-    },
-  },
 };
 </script>
 
