ae904a261e5003c38edcbe2649ba1b54da74493d
Viktor Abishev <abw00001@ya.ru>
2021-02-03 09:24:26 +0300

    Added multiworkers on prod

--- backend/scripts/start_prod.sh
+++ backend/scripts/start_prod.sh
@@ -4,4 +4,4 @@ python3 manage.py collectstatic --no-input
 python3 manage.py makemigrations
 python3 manage.py migrate --no-input
 python3 manage.py createsuperuser --no-input
-gunicorn backend.wsgi -b 0.0.0.0:8000
+gunicorn backend.wsgi -b 0.0.0.0:8000 -w 4
