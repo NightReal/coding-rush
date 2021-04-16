8fdda73a0abee871c1034081dc2c0c18e8489111
crazyilian <ilian.and.04@gmail.com>
2021-04-16 22:37:31 +0300

    feat: Upper progress bar moved to separate component.

---
+++ frontend/src/components/PageLoader.vue
@@ -0,0 +1,12 @@
+<template>
+  <v-progress-linear color="accent" :active="loading" :indeterminate="loading"
+                     style="position: fixed; z-index: 5; top:0; left: 0; right: 0;">
+  </v-progress-linear>
+</template>
+
+<script>
+export default {
+  name: 'PageLoader',
+  props: ['loading'],
+};
+</script>
