b7c35fa138d959a6d107988416f20146d52e89ec
Viktor <abw00001@ya.ru>
2021-04-21 13:58:54 +0300

    [feat] Lesson view now retrieves all attempt on this lesson

--- backend/lessons/views.py
+++ backend/lessons/views.py
@@ -1,19 +1,39 @@
-from rest_framework import generics, permissions
+from rest_framework import (
+    generics,
+    permissions,
+    response,
+    status,
+    views,
+)
 from .serializers import (
     LessonSerializer,
     LessonListSerializer,
 )
 from .models import (
     Lesson,
+    Attempt,
+)
+from django.db.models import (
+    Prefetch,
 )
 
 
 # Create your views here.
 
-class GetLessonView(generics.RetrieveAPIView):
-    queryset = Lesson.objects.all()
-    serializer_class = LessonSerializer
-    permission_classes = [permissions.AllowAny, ]
+class GetLessonView(views.APIView):
+    permission_classes = [permissions.IsAuthenticated, ]
+
+    def get(self, request, pk: int, *args, **kwargs):
+        user = request.user
+        # IDK, maybe this is slow, but I didn't found better solution. See LessonSerializer for better understanding of this hack.
+        # Seems like that currently it executes 3 SQL queries and it is the fastest possible result with current models
+        prefetch = Prefetch('attempts', queryset=Attempt.objects.filter(user_id=user.id), to_attr='cur_user_attempts')
+        # don't use get() here because it throws exception when there is no lesson with such id
+        lesson = Lesson.objects.prefetch_related(prefetch).filter(id=pk).first()
+        if lesson is None:
+            return response.Response(status=status.HTTP_404_NOT_FOUND)
+        serializer = LessonSerializer(lesson)
+        return response.Response(serializer.data)
 
 
 class GetAllLessonsView(generics.ListAPIView):
