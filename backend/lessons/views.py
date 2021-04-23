43659f167d8f8f0938311b08c5bf1ae2e1e4df3d
Viktor <abw00001@ya.ru>
2021-04-23 14:35:28 +0300

    [fix] Best attempt is now not in list

--- backend/lessons/views.py
+++ backend/lessons/views.py
@@ -15,6 +15,7 @@ from .models import (
 )
 from django.db.models import (
     Prefetch,
+    Max,
 )
 
 
@@ -32,7 +33,7 @@ class GetLessonView(views.APIView):
         lesson = Lesson.objects.prefetch_related(prefetch).filter(id=pk).first()
         if lesson is None:
             return response.Response(status=status.HTTP_404_NOT_FOUND)
-        serializer = LessonSerializer(lesson)
+        serializer = LessonSerializer(lesson, read_only=True)
         return response.Response(serializer.data)
 
 
@@ -42,8 +43,16 @@ class GetAllLessonsView(views.APIView):
     def get(self, request, *args, **kwargs):
         user = request.user
         # see above
+        # FIXME: rewrite this bullshit.
+        # to_attr is simply a list, and used as source in serializer.
+        # means that we can just do some magic and put best_user_attempt quicker than here and without postprocessing to single-model
         prefetch = Prefetch('attempts', queryset=Attempt.objects.filter(user_id=user.id).order_by('lesson_id', '-score').distinct('lesson_id'),
                             to_attr='best_user_attempt')
-        lesson = Lesson.objects.prefetch_related(prefetch).prefetch_related('codes').all()
-        serializer = LessonListSerializer(lesson, many=True)
+        lessons = Lesson.objects.prefetch_related(prefetch).prefetch_related('codes').all()
+        for lesson in lessons:
+            if len(lesson.best_user_attempt):
+                lesson.best_user_attempt = lesson.best_user_attempt[0]
+            else:
+                del lesson.best_user_attempt
+        serializer = LessonListSerializer(lessons, many=True, read_only=True)
         return response.Response(serializer.data)
