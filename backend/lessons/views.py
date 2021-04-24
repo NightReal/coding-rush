365fc2ed41d03b27d658c440bb8fd704a37483f8
Viktor <abw00001@ya.ru>
2021-04-24 09:52:34 +0300

    [refactor] Refactoring database bullshit

--- backend/lessons/views.py
+++ backend/lessons/views.py
@@ -26,13 +26,10 @@ class GetLessonView(views.APIView):
 
     def get(self, request, pk: int, *args, **kwargs):
         user = request.user
-        # IDK, maybe this is slow, but I didn't found better solution. See LessonSerializer for better understanding of this hack.
-        # Seems like that currently it executes 3 SQL queries and it is the fastest possible result with current models
-        prefetch = Prefetch('attempts', queryset=Attempt.objects.filter(user_id=user.id), to_attr='cur_user_attempts')
-        # don't use get() here because it throws exception when there is no lesson with such id
-        lesson = Lesson.objects.prefetch_related(prefetch).filter(id=pk).first()
+        lesson = Lesson.objects.filter(id=pk).first()
         if lesson is None:
             return response.Response(status=status.HTTP_404_NOT_FOUND)
+        lesson.cur_user_attempts = Attempt.objects.filter(user_id=user.id, lesson_id=pk)
         serializer = LessonSerializer(lesson, read_only=True)
         return response.Response(serializer.data)
 
@@ -42,17 +39,11 @@ class GetAllLessonsView(views.APIView):
 
     def get(self, request, *args, **kwargs):
         user = request.user
-        # see above
-        # FIXME: rewrite this bullshit.
-        # to_attr is simply a list, and used as source in serializer.
-        # means that we can just do some magic and put best_user_attempt quicker than here and without postprocessing to single-model
-        prefetch = Prefetch('attempts', queryset=Attempt.objects.filter(user_id=user.id).order_by('lesson_id', '-score').distinct('lesson_id'),
-                            to_attr='best_user_attempt')
-        lessons = Lesson.objects.prefetch_related(prefetch).prefetch_related('codes').all()
+        prefetch = Prefetch('attempts', queryset=Attempt.objects.filter(user_id=user.id).order_by('-score'))
+        lessons = Lesson.objects.prefetch_related(prefetch).prefetch_related('codes')
         for lesson in lessons:
-            if len(lesson.best_user_attempt):
-                lesson.best_user_attempt = lesson.best_user_attempt[0]
-            else:
-                del lesson.best_user_attempt
+            best = lesson.attempts.first()
+            if best:
+                lesson.best_user_attempt = best
         serializer = LessonListSerializer(lessons, many=True, read_only=True)
         return response.Response(serializer.data)
