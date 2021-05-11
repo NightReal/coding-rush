83a648881259d008ce8c4a615fa81f328e2084c5
crazyilian <ilian.and.04@gmail.com>
2021-05-11 22:26:02 +0300

    [fix][refactor]: Score formula

--- backend/lessons/utils/scoring.py
+++ backend/lessons/utils/scoring.py
@@ -1,2 +1,8 @@
+from math import round
+
 def score(speed: int, accuracy: float, difficulty: int):
-    return int(((speed * accuracy / 40000 + 1) ** ((difficulty / 6) ** 0.05) - 1) * 100)
+    norm_cpm = speed / 400
+    norm_acc = accuracy / 100
+    norm_dif = difficulty / 10
+    k = (norm_cpm * norm_acc + 1) ** (norm_dif ** 0.05 + (0.1 / norm_cpm)) - 1
+    return round(k * 100)
