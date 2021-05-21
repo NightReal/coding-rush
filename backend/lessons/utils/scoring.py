90f3e1d9cbb6d50c0654960a3bdd5c1d945571d2
crazyilian <ilian.and.04@gmail.com>
2021-05-21 10:25:37 +0300

    [fix]: score formula

--- backend/lessons/utils/scoring.py
+++ backend/lessons/utils/scoring.py
@@ -1,5 +1,5 @@
 def score(speed: int, accuracy: float, difficulty: int):
-    norm_cpm = speed / 360
+    norm_cpm = speed / 375
     norm_acc = accuracy / 100
     norm_dif = difficulty / 10
     k = (norm_cpm * (norm_acc + 1) + 1) ** (norm_dif ** 0.05 + (0.1 / norm_cpm)) - 1 - norm_cpm
