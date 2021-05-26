79d820f9060462a0149714ec98cdef11917d3ea5
Viktor Abishev <abw00001@ya.ru>
2021-05-26 14:21:57 +0300

    [refactor] Refactoring score

--- backend/lessons/utils/scoring.py
+++ backend/lessons/utils/scoring.py
@@ -5,10 +5,10 @@ def score(speed: int, accuracy: float, difficulty: int):
 
     base = cpm_norm * acc_norm
     if base <= 1:
-        pow = 0.9 * (diff_norm + 0.2) ** 3 + 0.1
+        p = 0.9 * (diff_norm + 0.2) ** 3 + 0.1
     else:
-        pow = (diff_norm + 0.5) ** 0.3
-    score_norm = base ** pow
+        p = (diff_norm + 0.5) ** 0.3
+    score_norm = base ** p
 
     score = round(score_norm * 100)
     return score
